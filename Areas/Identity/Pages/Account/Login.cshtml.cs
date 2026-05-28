1b2969141cb079fc7b79628f0ad46f10bbaabc4e
Bergamin <italobergamin321@gmail.com>
2026-05-28 09:19:33 -0400

    login publicador de ata

--- Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -66,7 +66,7 @@ namespace PublicadorDeAtas.Areas.Identity.Pages.Account
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
             [Required]
-            [EmailAddress]
+            [Display(Name = "CPF ou E-mail")]
             public string Email { get; set; }
 
             /// <summary>
